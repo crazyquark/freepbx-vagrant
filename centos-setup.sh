ba8f025511685fd34f9dcf9f7d0b8b91adeb044e
crazyquark <cristian.sandu@gmail.com>
2020-07-01 21:32:52 +0300

    typo

--- centos-setup.sh
+++ centos-setup.sh
@@ -43,7 +43,7 @@ systemctl enable mariadb.service
 systemctl start mariadb
 
 # Instead of mysql_secure_installation
-myql --user=root <<_EOF_
+mysql --user=root <<_EOF_
 UPDATE mysql.user SET Password=PASSWORD('') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
